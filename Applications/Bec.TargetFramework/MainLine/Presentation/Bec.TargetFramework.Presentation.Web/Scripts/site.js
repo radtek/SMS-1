ec77f865136f4f4ca1fecc164f1180f290a02d0e
Keith Howie <k.howie@beconsultancy.co.uk>
2015-04-23 11:34:31 +0100

    Tidied up temp companies page, new js to handle tabs & grids

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Scripts/site.js
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Scripts/site.js
@@ -51,8 +51,8 @@ function dateString(date) {
     }
 }
 
-function saveGridSort(gridElementId) {
-    var sort = $("#" + gridElementId).data("kendoGrid").getOptions().dataSource.sort;
+function saveGridSort(grid, gridElementId) {
+    var sort = grid.getOptions().dataSource.sort;
     sessionStorage["gridSort-" + gridElementId] = JSON.stringify(sort);
 }
 
@@ -64,3 +64,116 @@ function loadGridSort(gridElementId) {
         return null;
     }
 }
+
+//sets up tab page click events & loads relevant grids once tabs are shown.
+var tabItem = function (tabId, tabs) {
+    this.tabId = tabId;
+    this.tabs = tabs;
+    var self = this;
+
+    this.makeTab = function () {
+        var allTabElements = $('#' + this.tabId + ' a');
+
+        allTabElements.click(function (e) {
+            e.preventDefault()
+            $(this).tab('show')
+        });
+
+        allTabElements.on('shown.bs.tab', function (e) {
+            for (var t in self.tabs) {
+                if ($(e.target).attr('href') == '#' + t) {
+                    for (var g in self.tabs[t].grids) self.tabs[t].grids[g].makeGrid();
+                }
+            }
+        });
+    };
+
+    this.showTab = function (index) {
+        $('#' + this.tabId + ' li:eq(' + index + ') a').tab('show');
+    };
+
+};
+
+//sets up a grid, including:
+// initial load
+// persisting sorted columns
+// jumping to a particular row following an action
+// maintaining selection on sort
+var gridItem = function (options) {
+    this.options = options;
+    this.loaded = false;
+    this.selectedItem = null;
+    this.grid = null;
+    var self = this;
+
+    //pass through options to kendo and hook up events
+    this.makeGrid = function () {
+        if (this.loaded) return;
+        this.loaded = true;
+        this.grid = $("#" + this.options.gridElementId).kendoGrid({
+            dataSource: {
+                transport: {
+                    read: getGridDataFromUrl(this.options.url)
+                },
+                schema: this.options.schema,
+                sort: loadGridSort(this.options.gridElementId) || this.options.defaultSort
+            },
+            height: 300,
+            selectable: "row",
+            filterable: false,
+            sortable: true,
+            navigatable: true,
+            pageable: {
+                numeric: false,
+                previousNext: false,
+                messages: { display: "{2} rows" }
+            },
+            columns: options.columns,
+            dataBound: this.dataBound,
+            change: this.change
+        }).data("kendoGrid");
+    };
+
+    this.dataBound = function (e) {
+        saveGridSort(self.grid, self.options.gridElementId);
+
+        var gridData = self.grid.dataSource.data();
+        if (gridData.length == 0) return;
+
+        if (self.options.jumpToId != null && self.options.jumpToId != "") {
+            for (var i = 0; i < gridData.length; i++) {
+                if (self.options.jumpToId.replace(/-/g, "") == gridData[i].OrganisationID) {
+                    self.scrollToRow(gridData[i]);
+                    self.options.jumpToId = null; //make sure this is one off
+                    break;
+                }
+            }
+        } else {
+            self.scrollToRow(self.selectedItem);
+        }
+    };
+
+    this.change = function (e) {
+        var selectedRows = self.grid.select();
+        if (selectedRows.length == 1) {
+            var dataItem = self.grid.dataItem(selectedRows[0]);
+            self.selectedItem = dataItem;
+            self.options.change(dataItem); //any custom data binding
+        }
+    };
+
+    this.scrollToRow = function(item) {
+        if (!this.grid || !item) return;
+        var row = this.grid.tbody.find("tr[data-uid='" + item.uid + "']");
+        this.grid.select(row);
+
+        var scrollContentOffset = this.grid.tbody.offset().top;
+        var selectContentOffset = this.grid.select().offset().top;
+        var distance = selectContentOffset - scrollContentOffset;
+
+        this.grid.content.animate({
+            scrollTop: distance
+        }, 400);
+    };
+}
+
