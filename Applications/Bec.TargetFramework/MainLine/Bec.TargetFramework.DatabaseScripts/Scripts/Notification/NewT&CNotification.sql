2c55e22d9f1259c99c6b6dcf963521c5f79f0d9d
Keith Howie <k.howie@beconsultancy.co.uk>
2015-06-01 13:18:12 +0100

    New script to create new notification version

---
+++ Applications/Bec.TargetFramework/MainLine/Bec.TargetFramework.DatabaseScripts/Scripts/Notification/NewT&CNotification.sql
@@ -0,0 +1,9 @@
+﻿DO $$
+DECLARE ID UUID;
+BEGIN
+
+ID := (select dorg."NotificationConstructTemplateID" from "NotificationConstructTemplate" dorg
+	where dorg."Name" = 'TcPublic' limit 1);
+
+perform  public."fn_PromoteNotificationConstructTemplate"(ID,1);
+END $$;
