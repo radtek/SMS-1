a2a50f61ce60190e7e486c92b8eab676ca7d3cd6
Zenon Motyka <zenonm@beconsultancy.co.uk>
2015-07-29 09:29:45 +0100

    Added a mock for notification popup

---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Controllers/InternalNotificationsController.cs
@@ -0,0 +1,13 @@
+﻿using Bec.TargetFramework.Presentation.Web.Base;
+using System.Web.Mvc;
+
+namespace Bec.TargetFramework.Presentation.Web.Controllers
+{
+    public class InternalNotificationsController : ApplicationControllerBase
+    {
+        public PartialViewResult LatestNotifications()
+        {
+            return PartialView("_LatestNotifications");
+        }
+    }
+}
