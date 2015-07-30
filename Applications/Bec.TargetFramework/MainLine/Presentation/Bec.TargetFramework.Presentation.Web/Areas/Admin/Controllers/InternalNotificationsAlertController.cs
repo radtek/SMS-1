0d634035a274dd2887984d6d37d1ad1e3415f959
Zenon Motyka <zenonm@beconsultancy.co.uk>
2015-07-30 10:59:11 +0100

    Updated the notification popup and list of notifications in a separate tab

---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/InternalNotificationsAlertController.cs
@@ -0,0 +1,24 @@
+﻿using Bec.TargetFramework.Entities;
+using Bec.TargetFramework.Presentation.Web.Base;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Controllers
+{
+    public class InternalNotificationsAlertController : ApplicationControllerBase
+    {
+        public PartialViewResult LatestNotifications()
+        {
+            var data = Enumerable.Range(1, 20).Select(n =>
+                new VNotificationInternalUnreadDTO
+                {
+                    NotificationID = Guid.NewGuid(),
+                    Name = "Notification " + n + " firm X marked the account Y as suspicious fraud.",
+                    DateSent = DateTime.Now.AddMinutes(n * (-2))
+                }).ToList();
+
+            return PartialView("_LatestNotifications", data);
+        }
+    }
+}
---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/InternalNotificationsController.cs
@@ -0,0 +1,44 @@
+﻿using Bec.TargetFramework.Entities;
+using Bec.TargetFramework.Presentation.Web.Base;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Controllers
+{
+    public class InternalNotificationsController : ApplicationControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult LoadNotifications()
+        {
+            var data = Enumerable.Range(1, 40).Select(n =>
+                new VNotificationInternalUnreadDTO
+                {
+                    NotificationID = Guid.NewGuid(),
+                    Name = "Notification " + n + " firm X marked the account Y as suspicious fraud.",
+                    DateSent = DateTime.Now.AddMinutes(n * (-2))
+                });
+                //new NotificationEntry
+                //{
+                //    NotificationID = Guid.NewGuid(),
+                //    NotificationSubject = "Notification " + n + " firm X marked the account Y as suspicious fraud.",
+                //    DateSent = DateTime.Now.AddMinutes(n * (-2))
+                //});
+
+            var jsonData = new { total = data.Count(), data };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+    public class NotificationEntry
+    {
+        public Guid NotificationID { get; set; }
+        public string NotificationSubject { get; set; }
+        public DateTime DateSent { get; set; }
+
+    }
+}
