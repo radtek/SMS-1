ac2096b186555daa822c274eb5c4debffbc146ca
Zenon Motyka <zenonm@beconsultancy.co.uk>
2015-07-31 11:00:21 +0100

    Added initial bank account marked as fraud notification

---
+++ Applications/Bec.TargetFramework/MainLine/SB/Bec.TargetFramework.SB.Notifications/Mutators/BankAccountMarkedAsFraudSuspiciousMutator.cs
@@ -0,0 +1,17 @@
+﻿using Bec.TargetFramework.Entities;
+using Bec.TargetFramework.SB.Notifications.Base;
+
+namespace Bec.TargetFramework.SB.Notifications.Mutators
+{
+    public sealed class BankAccountMarkedAsFraudSuspiciousMutator : BaseNotificationMutator
+    {
+        public override NotificationDictionaryDTO MutateNotification()
+        {
+            return NotificationDictionary;
+        }
+
+        public override void InitialiseMutator()
+        {
+        }
+    }
+}
---
+++ Applications/Bec.TargetFramework/MainLine/Shared/Bec.TargetFramework.Entities/DTO/Notification/BankAccountMarkedAsFraudSuspiciousDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Bec.TargetFramework.Entities.DTO.Notification
+{
+    [DataContract]
+    public class BankAccountMarkedAsFraudSuspiciousDTO
+    {
+        [DataMember]
+        public string SortCode { get; set; }
+        [DataMember]
+        public string AccountNumber { get; set; }
+    }
+}
