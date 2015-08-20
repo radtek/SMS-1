99495dcfebe3df8a56ef0bb4e9eda9de11dfa708
unknown <zenonm@BEC-DEVM-04.bec.local>
2015-08-20 09:13:57 +0100

    Refactored date range picker to jquery plugin

---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Scripts/Bec/bec.daterange.js
@@ -0,0 +1,53 @@
+﻿(function ($) {
+    $.extend({
+        becDateRange: function (options) {
+            var dateTo = new Date();
+            var dateFrom = new Date(dateTo.getFullYear() - 1, dateTo.getMonth(), dateTo.getDate());
+
+            // defaults
+            var settings = $.extend({
+                dateFromInputSelector: "",
+                dateToInputSelector: "",
+                dateFromShowButtonSelector: "",
+                dateToShowButtonSelector: "",
+                dateFormat: "dd/mm/yy",
+                dateFrom: dateFrom,
+                dateTo: dateTo,
+                maxDateTo: dateTo
+            }, options);
+
+            // Date Range Picker
+            $(settings.dateFromInputSelector).datepicker({
+                defaultDate: settings.dateFrom,
+                dateFormat: settings.dateFormat,
+                changeMonth: true,
+                showButtonPanel: true,
+                prevText: "<i class=\"fa fa-chevron-left\"></i>",
+                nextText: "<i class=\"fa fa-chevron-right\"></i>",
+                onClose: function (selectedDate) {
+                    $(settings.dateToInputSelector).datepicker("option", "minDate", selectedDate);
+                }
+            }).datepicker("setDate", settings.dateFrom);
+
+            $(settings.dateToInputSelector).datepicker({
+                defaultDate: settings.dateTo,
+                dateFormat: settings.dateFormat,
+                changeMonth: true,
+                showButtonPanel: true,
+                maxDate: dateTo,
+                prevText: "<i class=\"fa fa-chevron-left\"></i>",
+                nextText: "<i class=\"fa fa-chevron-right\"></i>",
+                onClose: function (selectedDate) {
+                    $(settings.dateFromInputSelector).datepicker("option", "maxDate", selectedDate);
+                }
+            }).datepicker("setDate", settings.dateTo);
+
+            $(settings.dateFromShowButtonSelector).click(function () {
+                $(settings.dateFromInputSelector).datepicker("show");
+            });
+            $(settings.dateToShowButtonSelector).click(function () {
+                $(settings.dateToInputSelector).datepicker("show");
+            });
+        }
+    });
+})(jQuery);
