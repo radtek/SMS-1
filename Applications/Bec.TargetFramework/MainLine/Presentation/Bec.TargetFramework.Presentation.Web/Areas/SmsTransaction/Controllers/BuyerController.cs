07a33a20cd9edaed328b9919c678fae705e42b31
Keith Howie <k.howie@beconsultancy.co.uk>
2015-08-28 13:22:25 +0100

    Update Becky's homepage

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/SmsTransaction/Controllers/BuyerController.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/SmsTransaction/Controllers/BuyerController.cs
@@ -16,7 +16,17 @@ namespace Bec.TargetFramework.Presentation.Web.Areas.SmsTransaction.Controllers
         public async Task<ActionResult> Index()
         {
             var uaoID = WebUserHelper.GetWebUserObject(HttpContext).UaoID;
-            var select = ODataHelper.Select<SmsTransactionDTO>(x => new { x.OrganisationID, Names = x.Organisation.OrganisationDetails.Select(y => new { y.Name }) });
+            var select = ODataHelper.Select<SmsTransactionDTO>(x => new
+            {
+                x.OrganisationID,
+                x.Address.Line1,
+                x.Address.Line2,
+                x.Address.Town,
+                x.Address.City,
+                x.Address.County,
+                x.Address.PostalCode,
+                Names = x.Organisation.OrganisationDetails.Select(y => new { y.Name })
+            });
             var filter = ODataHelper.Filter<SmsTransactionDTO>(x => x.UserAccountOrganisationID == uaoID);
             var data = await QueryClient.QueryAsync<SmsTransactionDTO>("SmsTransactions", select + filter);
             return View(data);
