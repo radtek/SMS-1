1a4f8a5a8f9fec691bcf3c86b36b2046f23f25e0
Keith Howie <k.howie@beconsultancy.co.uk>
2015-09-11 08:40:38 +0100

    Filter to set response headers

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/App_Start/FilterConfig.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/App_Start/FilterConfig.cs
@@ -9,7 +9,7 @@ namespace Bec.TargetFramework.Presentation.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new ContentSecurityPolicyFilterAttribute());
+            filters.Add(new SecurityFilterAttribute());
         }
     }
 }
