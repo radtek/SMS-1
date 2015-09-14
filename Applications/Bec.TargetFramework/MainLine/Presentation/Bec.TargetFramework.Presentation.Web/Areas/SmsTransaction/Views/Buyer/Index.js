bb124b9a1ce2d86de32249c00546689aa7cd846b
Keith Howie <k.howie@beconsultancy.co.uk>
2015-09-14 09:49:11 +0100

    Bank account check screen bug fixes

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/SmsTransaction/Views/Buyer/Index.js
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/SmsTransaction/Views/Buyer/Index.js
@@ -1,5 +1,5 @@
-﻿$(function(){
-    $('.check-bank-form').each(function (i, item) {
+﻿$(function () {    
+    $('.bank-check-form').each(function (i, item) {
         $(item).validate({
             ignore: '.skip',
             // Rules for form validation
@@ -18,39 +18,39 @@
             submitHandler: validateSubmit
         });
     });
-
-    function validateSubmit(form) {
-        $("#submitButton").prop('disabled', true);
-
-        var matchDiv = $('#result-match-' + $(form).data("index"));
-        var noMatchDiv = $('#result-no-match-' + $(form).data("index"));
-        var serverErrorDiv = $('#result-server-error-' + $(form).data("index"));
-        var spinner = $('#spinner-' + $(form).data("index"));
-
-        matchDiv.hide();
-        noMatchDiv.hide();
-        serverErrorDiv.hide();
-        spinner.show();
-
-        var formData = $(form).serializeArray();
-        ajaxWrapper({
-            url: $(form).data("url"),
-            type: "POST",
-            data: formData
-        }).done(function (res) {
-            $("#submitButton").prop('disabled', false);
-            spinner.hide();
-            if (res.result == true) {
-                matchDiv.show();
-            }
-            else {
-                noMatchDiv.show();
-            }
-        }).fail(function (e) {
-            $("#submitButton").prop('disabled', false);
-            console.log(e);
-            spinner.hide();
-            serverErrorDiv.show();
-        });
-    }
 });
+
+function validateSubmit(form) {
+    $(".check-button").prop('disabled', true);
+
+    var matchDiv = $('#result-match-' + $(form).data("index"));
+    var noMatchDiv = $('#result-no-match-' + $(form).data("index"));
+    var serverErrorDiv = $('#result-server-error-' + $(form).data("index"));
+    var spinner = $('#spinner-' + $(form).data("index"));
+
+    matchDiv.hide();
+    noMatchDiv.hide();
+    serverErrorDiv.hide();
+    spinner.show();
+
+    var formData = $(form).serializeArray();
+    ajaxWrapper({
+        url: $(form).data("url"),
+        type: "POST",
+        data: formData
+    }).done(function (res) {
+        $(".check-button").prop('disabled', false);
+        spinner.hide();
+        if (res.result == true) {
+            matchDiv.show();
+        }
+        else {
+            noMatchDiv.show();
+        }
+    }).fail(function (e) {
+        $(".check-button").prop('disabled', false);
+        console.log(e);
+        spinner.hide();
+        serverErrorDiv.show();
+    });
+};
