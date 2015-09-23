c7e45dcce45473e4c3fb35d791a1452e3e295c99
unknown <zenonm@BEC-DEVM-04.bec.local>
2015-09-23 13:44:05 +0100

    Added the parameters for adding Ana

--- Applications/Bec.TargetFramework/MainLine/Tests/Bec.TargetFramework.TestsService/Controllers/UserController.cs
+++ Applications/Bec.TargetFramework/MainLine/Tests/Bec.TargetFramework.TestsService/Controllers/UserController.cs
@@ -5,30 +5,24 @@ using System.Web.Http;
 using System.Linq;
 using EnsureThat;
 using System;
+using Bec.TargetFramework.Entities.Enums;
+using Bec.TargetFramework.Infrastructure.Extensions;
 
 namespace Bec.TargetFramework.TestsService.Controllers
 {
     public class UserController : ApiController
     {
         public IDbContextScopeFactory DbContextScopeFactory { get; set; }
-        public async Task<bool> AddAna()
+        public async Task<bool> AddAna(string username, string email)
         {
-            //Guid? organisationId = null;
-            //using (var scope = DbContextScopeFactory.Create())
-            //{
-            //    organisationId = scope.DbContexts.Get<TargetFrameworkEntities>()
-            //        .FnCreateOrganisationFromDefault(31, Guid.Parse("6cd2a94e-61da-11e5-9721-00155d0a147a"), 1, "Anas Company",
-            //        "Anas Company Description", "Test");
-            //    Ensure.That(organisationId).IsNotNull();
-            //}
-            
             var result = false;
+
             var anaUserId = Guid.NewGuid();
             var anaUserContactId = Guid.NewGuid();
             var addressId = Guid.NewGuid();
             var organisationContactId = Guid.NewGuid();
-            var workAddressTypeId = 4974; //Work
-            int? professionalOrganisationType = 31; // ProfessionalOrganisation
+            var workAddressTypeId = AddressTypeIDEnum.Work.GetIntValue();
+            var professionalOrganisationType = OrganisationTypeEnum.Professional.GetIntValue();
             var organisationAdministratorUserTypeId = Guid.Parse("62885ba9-36ba-4035-836b-8e0c127098a2"); // OrganisationAdministrator
             Guid? defaultOrganisationId;
             Guid? organisationId = null;
@@ -89,8 +83,8 @@ namespace Bec.TargetFramework.TestsService.Controllers
                 {
                     ID = anaUserId,
                     Tenant = "default",
-                    Username = "Ana",
-                    Email = "ana.qualified.administrator@beconsultancy.co.uk",
+                    Username = username,
+                    Email = email,
                     Created = now,
                     LastUpdated = now,
                     RequiresPasswordReset = false, 
@@ -101,7 +95,7 @@ namespace Bec.TargetFramework.TestsService.Controllers
                     VerificationPurpose = 3,
                     VerificationKeySent = now,
                     HashedPassword = "1F400.AF6d8CXT2jJcNPFdqPv7LLFxKZAzklBhi2/XWnd6+nMvLWDTZ6ejcLRVKCWk0eRtbw==",
-                    VerificationStorage = "ana.qualified.administrator@beconsultancy.co.uk",
+                    VerificationStorage = email,
                     IsActive = true,
                     IsDeleted = false, 
                     IsTemporaryAccount = false, 
@@ -135,7 +129,7 @@ namespace Bec.TargetFramework.TestsService.Controllers
                     Telephone1 = "123455621",
                     FirstName = "Ana",
                     LastName = "Qualified Administrator",
-                    EmailAddress1 = "ana.qualified.administrator@beconsultancy.co.uk",
+                    EmailAddress1 = email,
                     Salutation = "Mrs"
                 };
                 scope.DbContexts.Get<TargetFrameworkEntities>().Contacts.Add(anaContact);
@@ -153,181 +147,3 @@ namespace Bec.TargetFramework.TestsService.Controllers
         }
     }
 }
-
-//DECLARE AnaUserId uuid := (select uuid_generate_v1());
-//DECLARE AnaUserContactId uuid := (select uuid_generate_v1());
-//DECLARE AnaUserAccountOrganisationId uuid;
-
-//DECLARE AddressId uuid := (select uuid_generate_v1());
-//DECLARE OrganisationContactId uuid := (select uuid_generate_v1());
-//DECLARE DefaultOrganisationId uuid := (
-//    SELECT "DefaultOrganisationID"
-//    FROM "DefaultOrganisation"
-//    WHERE "Name" LIKE 'Professional Organisation'
-//    LIMIT 1
-//);
-
-//DECLARE OrganisationId uuid;
-//DECLARE WorkAddressTypeId integer := 4974; -- Work
-//DECLARE ProfessionalOrganisationType integer := 31; -- ProfessionalOrganisation
-//DECLARE OrganisationAdministratorUserTypeId uuid := '62885ba9-36ba-4035-836b-8e0c127098a2'; -- OrganisationAdministrator
-//DECLARE OrganisationEmployeeRoleId uuid;
-//BEGIN
-//    -- ======================== ORGANISATION ========================
-
-//    --  ============ CREATE PROFESSIONAL ORGANISATION  ============
-//    OrganisationId := "fn_CreateOrganisationFromDefault"(ProfessionalOrganisationType, DefaultOrganisationId, 1, 'Default Company', 'Default Company Description', 'AutomatedScript');
-//    OrganisationEmployeeRoleId := (
-//        SELECT "OrganisationRoleID"
-//        FROM "OrganisationRole"
-//        WHERE "RoleName" LIKE 'Organisation Employee'
-//        LIMIT 1
-//    );
-
-//    -- ============ CREATE CONTACT FOR ORGANISATION  ============
-//    INSERT INTO public."Contact"(
-//        "ContactID",
-//        "ContactName",
-//        "ParentID",
-//        "IsPrimaryContact"
-//    )
-//    VALUES (
-//        OrganisationContactId,
-//        'Default Organisation Contact',
-//        OrganisationId,
-//        TRUE
-//    );
-//    INSERT INTO public."ContactRegulator"
-//    (
-//        "ContactID",
-//        "RegulatorNumber",
-//        "RegulatorName",
-//        "RegulatorOtherName"
-//    )
-//    VALUES (
-//        OrganisationContactID,
-//        '123456',
-//        'SRI',
-//        'No Name'
-//    );
-//    INSERT INTO public."Address"
-//    (
-//        "AddressID",
-//        "Name",
-//        "Line1",
-//        "Line2",
-//        "County",
-//        "PostalCode",
-//        "ParentID",
-//        "AddressTypeID",
-//        "IsPrimaryAddress",
-//        "AdditionalAddressInformation"
-//    )
-//    VALUES (
-//        AddressId,
-//        'Default Address',
-//        'Default Address Line 1',
-//        'Default Address Line 2',
-//        'Default Address County',
-//        'AA23 D34',
-//        OrganisationContactId,
-//        WorkAddressTypeId,
-//        TRUE,
-//        'Default Additional Address Information'
-//    );
-
-//    -- ======================== ANA ========================
-
-//    -- ============ CREATE ANA USER ACCOUNT ============
-//    INSERT INTO public."UserAccounts"
-//    (
-//        "ID",
-//        "Tenant",
-//        "Username",
-//        "Email",
-//        "Created",
-//        "LastUpdated",
-//        "RequiresPasswordReset",
-//        "IsAccountVerified",
-//        "IsLoginAllowed",
-//        "IsAccountClosed",
-//        "VerificationKey",
-//        "VerificationPurpose",
-//        "VerificationKeySent",
-//        "HashedPassword",
-//        "VerificationStorage",
-//        "IsActive",
-//        "IsDeleted",
-//        "IsTemporaryAccount",
-//        "CreatedOn",
-//        "IsApproved",
-//        "IsEmployee",
-//        "IsForgotUsernameRequestAllowed",
-//        "IsForgotPasswordRequestAllowed",
-//        "AccountTwoFactorAuthMode",
-//        "CurrentTwoFactorAuthStatus",
-//        "FailedLoginCount"
-//    )
-//    VALUES (
-//        AnaUserId,
-//        'default',
-//        'Ana',
-//        'ana.qualified.administrator@beconsultancy.co.uk',
-//        now(),
-//        now(),
-//        FALSE, -- RequiresPasswordReset
-//        TRUE, -- IsAccountVerified
-//        TRUE, -- IsLoginAllowed
-//        FALSE, -- IsAccountClosed
-//        '91F3AB4BDD90240A37BC4CE41F8B8AD55928AC19A484D112FBAE5A869684B468',
-//        3,
-//        now(),
-//        '1F400.AF6d8CXT2jJcNPFdqPv7LLFxKZAzklBhi2/XWnd6+nMvLWDTZ6ejcLRVKCWk0eRtbw==',
-//        'ana.qualified.administrator@beconsultancy.co.uk',
-//        TRUE, -- IsActive
-//        FALSE, -- IsDeleted
-//        FALSE, -- IsTemporaryAccount,
-//        now(),
-//        TRUE, -- IsApproved,
-//        FALSE, -- IsEmployee,
-//        TRUE, -- IsForgotUsernameRequestAllowed,
-//        TRUE, -- IsForgotPasswordRequestAllowed,
-//        0, -- AccountTwoFactorAuthMode
-//        0, -- "CurrentTwoFactorAuthStatus"
-//        0 -- "FailedLoginCount"
-//    );
-
-//    -- ============ CREATE ANA USER ACCOUNT ============
-//    -- Adds Ana to default roles
-//    AnaUserAccountOrganisationId := (SELECT "fn_AddUserToOrganisation"(AnaUserId, OrganisationId, OrganisationAdministratorUserTypeId, OrganisationId, TRUE));
-
-//    -- ============ UPDATE ASSIGNEMNT TO THE ORGANISATION
-//    INSERT INTO public."Contact"(
-//        "ContactID",
-//        "ContactName",
-//        "ParentID",
-//        "IsPrimaryContact",
-//        "Telephone1",
-//        "FirstName",
-//        "LastName",
-//        "EmailAddress1",
-//        "Salutation"
-//    )
-//    VALUES (
-//        AnaUserContactId,
-//        'Default Ana Contact',
-//        AnaUserAccountOrganisationId,
-//        TRUE,
-//        '123455621',
-//        'Ana',
-//        'Qualified Administrator',
-//        'ana.qualified.administrator@beconsultancy.co.uk',
-//        'Mrs'
-//    );
-//    UPDATE "UserAccountOrganisation"
-//    SET "PrimaryContactID" = AnaUserContactId
-//    WHERE "UserAccountOrganisationID" = AnaUserAccountOrganisationId;
-
-
-
-//END $$;
