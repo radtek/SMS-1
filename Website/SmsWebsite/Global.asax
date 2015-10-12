d474b0e302d8e2c4417687a79db56911512a5b11
Keith Howie <k.howie@beconsultancy.co.uk>
2015-10-12 11:30:58 +0100

    New Sms Website

---
+++ Website/SmsWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SmsWebsite.MvcApplication" Language="C#" %>
---
+++ Website/SmsWebsite/Helper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmsWebsite
+{
+    public static class Helper
+    {
+        public static string Conditional(this HtmlHelper htmlHelper, string output, string key, params string[] rh)
+        {
+            if (rh.Contains(htmlHelper.ViewContext.RouteData.Values[key].ToString()))
+                return output;
+            else
+                return "";
+        }
+    }
+}
