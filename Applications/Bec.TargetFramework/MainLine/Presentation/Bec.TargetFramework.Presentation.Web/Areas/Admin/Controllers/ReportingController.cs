c1bc08dcc377d7729f61c82e00fbf51e501e60d0
Keith Howie <k.howie@beconsultancy.co.uk>
2015-11-04 08:33:24 +0000

    Users and Firms reports

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/ReportingController.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/ReportingController.cs
@@ -25,77 +25,150 @@ namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Controllers
         {
         }
 
-        public ActionResult Index()
+        public ActionResult Transactions()
+        {
+            return View();
+        }
+
+        public ActionResult Firms()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
         {
             return View();
         }
 
         public async Task<ActionResult> GetData(string source, string group)
         {
-            string select;
             switch (source)
             {
-                case "SmsTransaction":
-                    select = ODataHelper.Select<SmsTransactionDTO>(x => new
-                    {
-                        Names = x.Organisation.OrganisationDetails.Select(b => new { b.Name }),
-                        States = x.Organisation.OrganisationStatus.Select(c => new { c.StatusTypeValue.Name, c.StatusChangedOn }),
-                        Users = x.SmsUserAccountOrganisationTransactions.Select(y => new
-                        {
-                            y.SmsUserAccountOrganisationTransactionTypeID,
-                            y.UserAccountOrganisation.UserAccount.IsTemporaryAccount,
-                            y.UserAccountOrganisation.UserAccount.Created,
-                            y.UserAccountOrganisation.UserAccount.AccountCreated
-                        })
-                    });
-
-                    var alltx = await queryClient.QueryAsync<SmsTransactionDTO>("SmsTransactions", select);
-                    int i = 0;
-                    var data = alltx.Select(d => new DataItem
-                    {
-                        Id = i++,
-                        OrganisationName = d.Organisation.OrganisationDetails.First().Name,
-                        PrimaryBuyer = d.SmsUserAccountOrganisationTransactions.FirstOrDefault(x => x.SmsUserAccountOrganisationTransactionTypeID == UserAccountOrganisationTransactionType.Buyer.GetIntValue())
-                    });
-
-                    Func<DataItem, bool> loggedinFunc = x => x.PrimaryBuyer != null && !x.PrimaryBuyer.UserAccountOrganisation.UserAccount.IsTemporaryAccount;
-                    Func<IEnumerable<DataItem>, TimeDetails> timeFunc = x =>
-                    {
-                        var withVals = x.Where(y =>
-                            y.PrimaryBuyer != null &&
-                            y.PrimaryBuyer.UserAccountOrganisation != null &&
-                            y.PrimaryBuyer.UserAccountOrganisation.UserAccount != null &&
-                            y.PrimaryBuyer.UserAccountOrganisation.UserAccount.AccountCreated.HasValue);
-                        if (withVals.Any())
-                            return formatMinutes(withVals.Select(z => z.PrimaryBuyer.UserAccountOrganisation.UserAccount.AccountCreated.Value - z.PrimaryBuyer.UserAccountOrganisation.UserAccount.Created).Average(t => t.TotalMinutes));
-                        else
-                            return new TimeDetails { Value = 0, Text = "-" };
-                    };
-
-                    Func<DataItem, string> getGroup = x =>
-                    {
-                        if (group == "[all]")
-                            return "(All Firms)";
-                        else if (group == "Firm")
-                            return x.OrganisationName;
-                        else
-                            return x.Id.ToString();
-                    };
-
-                    i = 0;
-                    var ret = data.GroupBy(getGroup).Select(g => new
-                    {
-                        Id = i++,
-                        Name = string.IsNullOrEmpty(group) ? g.First().OrganisationName : g.Key,
-                        Total = g.Count(),
-                        LoggedIn = g.Count(loggedinFunc),
-                        Time = timeFunc(g)
-                    });
-                    return Json(new { total = ret.Count(), list = ret }, JsonRequestBehavior.AllowGet);
-
-                default:
-                    throw new Exception();
+                case "SmsTransaction":return await TransactionReport(group);
+                case "Organisation": return await OrganisationReport(group);
+                case "User": return await UserReport(group);
+                default: throw new Exception();
             }
+        }
+
+        //pro users by organisation
+        private async Task<ActionResult> UserReport(string group)
+        {
+            var proOrgType = OrganisationTypeEnum.Professional.GetIntValue();
+            var select = ODataHelper.Select<UserAccountOrganisationDTO>(x => new
+            {
+                Names = x.Organisation.OrganisationDetails.Select(b => new { b.Name }),
+                x.UserAccount.Created,
+                x.UserAccount.AccountCreated
+            });
+            var filter = ODataHelper.Filter<UserAccountOrganisationDTO>(x => x.Organisation.OrganisationTypeID == proOrgType);
+            var allUsers = await queryClient.QueryAsync<UserAccountOrganisationDTO>("UserAccountOrganisations", select + filter);
+
+            Func<IEnumerable<UserAccountOrganisationDTO>, TimeDetails> timeFunc = x =>
+            {
+                var withVals = x.Where(y => y.UserAccount != null && y.UserAccount.AccountCreated.HasValue);
+                if (withVals.Any())
+                    return formatMinutes(withVals.Select(z => z.UserAccount.AccountCreated.Value - z.UserAccount.Created).Average(t => t.TotalMinutes));
+                else
+                    return new TimeDetails { Value = 0, Text = "-" };
+            };
+
+            int i = 0;
+            var ret = allUsers.GroupBy(x => group == "Firm" ? x.Organisation.OrganisationDetails.First().Name : "(All Firms)").Select(x => new
+            {
+                Id = i++,
+                Name = x.Key,
+                Total = x.Count(),
+                Time = timeFunc(x)
+            });
+
+            return Json(new { total = ret.Count(), list = ret }, JsonRequestBehavior.AllowGet);
+        }
+
+        private async Task<ActionResult> OrganisationReport(string group)
+        {
+            var proOrgType = OrganisationTypeEnum.Professional.GetIntValue();
+            var select = ODataHelper.Select<OrganisationDTO>(x => new {
+                Names = x.OrganisationDetails.Select(b => new { b.Name }),
+                Statii = x.OrganisationStatus.Select(s => new { s.StatusChangedOn, s.StatusTypeValue.Name})
+            });
+            var filter = ODataHelper.Filter<OrganisationDTO>(x => x.OrganisationTypeID == proOrgType);
+            var allOrgs = await queryClient.QueryAsync<OrganisationDTO>("Organisations", select + filter);
+            var data = allOrgs.Select(x => new {
+                Name = x.OrganisationDetails.First().Name,
+                Status = x.OrganisationStatus.OrderBy(y=>y.StatusChangedOn).Last().StatusTypeValue.Name
+            });
+
+            int i = 0;
+            var ret = data.GroupBy(x => group == "Status" ? x.Status : "(All Firms)").Select(x => new
+            {
+                Id = i++,
+                Name = x.Key,
+                Total = x.Count()
+            });
+
+            return Json(new { total = ret.Count(), list = ret }, JsonRequestBehavior.AllowGet);
+        }
+
+        //sms transactions including user time to log in
+        private async Task<ActionResult> TransactionReport(string group)
+        {
+            var select = ODataHelper.Select<SmsTransactionDTO>(x => new
+            {
+                Names = x.Organisation.OrganisationDetails.Select(b => new { b.Name }),
+                States = x.Organisation.OrganisationStatus.Select(c => new { c.StatusTypeValue.Name, c.StatusChangedOn }),
+                Users = x.SmsUserAccountOrganisationTransactions.Select(y => new
+                {
+                    y.SmsUserAccountOrganisationTransactionTypeID,
+                    y.UserAccountOrganisation.UserAccount.IsTemporaryAccount,
+                    y.UserAccountOrganisation.UserAccount.Created,
+                    y.UserAccountOrganisation.UserAccount.AccountCreated
+                })
+            });
+
+            var alltx = await queryClient.QueryAsync<SmsTransactionDTO>("SmsTransactions", select);
+            int i = 0;
+            var data = alltx.Select(d => new TxDataItem
+            {
+                Id = i++,
+                OrganisationName = d.Organisation.OrganisationDetails.First().Name,
+                PrimaryBuyer = d.SmsUserAccountOrganisationTransactions.FirstOrDefault(x => x.SmsUserAccountOrganisationTransactionTypeID == UserAccountOrganisationTransactionType.Buyer.GetIntValue())
+            });
+
+            Func<TxDataItem, bool> loggedinFunc = x => x.PrimaryBuyer != null && !x.PrimaryBuyer.UserAccountOrganisation.UserAccount.IsTemporaryAccount;
+            Func<IEnumerable<TxDataItem>, TimeDetails> timeFunc = x =>
+            {
+                var withVals = x.Where(y =>
+                    y.PrimaryBuyer != null &&
+                    y.PrimaryBuyer.UserAccountOrganisation != null &&
+                    y.PrimaryBuyer.UserAccountOrganisation.UserAccount != null &&
+                    y.PrimaryBuyer.UserAccountOrganisation.UserAccount.AccountCreated.HasValue);
+                if (withVals.Any())
+                    return formatMinutes(withVals.Select(z => z.PrimaryBuyer.UserAccountOrganisation.UserAccount.AccountCreated.Value - z.PrimaryBuyer.UserAccountOrganisation.UserAccount.Created).Average(t => t.TotalMinutes));
+                else
+                    return new TimeDetails { Value = 0, Text = "-" };
+            };
+
+            Func<TxDataItem, string> getGroup = x =>
+            {
+                if (group == "[all]")
+                    return x.Id.ToString();
+                else if (group == "Firm")
+                    return x.OrganisationName;
+                else
+                    return "(All Firms)";
+            };
+
+            i = 0;
+            var ret = data.GroupBy(getGroup).Select(g => new
+            {
+                Id = i++,
+                Name = string.IsNullOrEmpty(group) ? g.First().OrganisationName : g.Key,
+                Total = g.Count(),
+                LoggedIn = g.Count(loggedinFunc),
+                Time = timeFunc(g)
+            });
+            return Json(new { total = ret.Count(), list = ret }, JsonRequestBehavior.AllowGet);
         }
 
         private TimeDetails formatMinutes(double minutes)
@@ -112,7 +185,7 @@ namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Controllers
         }
     }
 
-    public class DataItem
+    public class TxDataItem
     {
         public int Id { get; set; }
         public string OrganisationName { get; set; }
---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Reporting/Users.js
@@ -0,0 +1,71 @@
+﻿var grid1, grid2, grid3;
+$(function () {
+    //set up grid options for the three grids. most are passed straight on to kendo grid.
+    grid1 = new gridItem(
+        {
+            gridElementId: 'grid1',
+            url: $('#grid1').data("url"),
+            schema: { data: "list", total: "total", model: { id: "Id" } },
+            columns: [
+                    {
+                        field: "Name",
+                        title: "Company Name"
+                    },
+                    {
+                        field: "Total",
+                        title: "Total"
+                    },
+                    {
+                        field: "Time.Value",
+                        title: "Time to register",
+                        template: function (dataItem) {
+                            return kendo.htmlEncode(dataItem.Time.Text);
+                        }
+                    }
+            ]
+        });
+
+    grid2 = new gridItem(
+        {
+            gridElementId: 'grid2',
+            url: $('#grid2').data("url"),
+            schema: { data: "list", total: "total", model: { id: "Id" } },
+            columns: [
+                    {
+                        field: "Name",
+                        title: "Company Name"
+                    },
+                    {
+                        field: "Total",
+                        title: "Total"
+                    },
+                    {
+                        field: "Time.Value",
+                        title: "Time to register",
+                        template: function (dataItem) {
+                            return kendo.htmlEncode(dataItem.Time.Text);
+                        }
+                    }
+            ]
+        });
+
+    //findModalLinks();
+    var tabs = new tabItem("myTab1",
+        {
+            s1: {
+                grids: [grid1]
+            },
+            s2: {
+                grids: [grid2]
+            }
+        });
+
+    //set up tabs
+    tabs.makeTab();
+    tabs.showTab($('#myTab1').data("selected"));
+
+    $('#sources input').change(function () {
+        var g = $('#reportGrid').data("kendoGrid");
+        g.dataSource.transport.options.read.url = $('#reportGrid').data("url") + "&group=" + $(this).data("group");
+    });
+});
