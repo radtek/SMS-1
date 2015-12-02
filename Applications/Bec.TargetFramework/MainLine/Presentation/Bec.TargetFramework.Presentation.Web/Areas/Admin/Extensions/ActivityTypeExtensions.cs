38cede6c7e0c68542e095140df906663337b7cae
Zenon Motyka <zenonm@beconsultancy.co.uk>
2015-12-02 14:39:57 +0000

    Added the activity specific icon for unread conversations

---
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Extensions/ActivityTypeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Bec.TargetFramework.Entities.Enums;
+
+namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Extensions
+{
+    public static class ActivityTypeExtensions
+    {
+        public static string GetFaIconClass(this ActivityType activityType)
+        {
+            switch (activityType)
+            {
+                case ActivityType.SmsTransaction:
+                    return "fa-home";
+                case ActivityType.BankAccount:
+                    return "fa-bank";
+                default:
+                    return "fa-envelope-o";
+            }
+        }
+    }
+}
