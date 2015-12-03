e394a809525b0796b5aafde516a577c2f13a82bb
Zenon Motyka <zenonm@beconsultancy.co.uk>
2015-12-03 08:18:13 +0000

    Disable reply for non-participants

--- Applications/Bec.TargetFramework/MainLine/Shared/Bec.TargetFramework.Entities/DTO/MessageContainerDTO.cs
+++ Applications/Bec.TargetFramework/MainLine/Shared/Bec.TargetFramework.Entities/DTO/MessageContainerDTO.cs
@@ -6,6 +6,7 @@ namespace Bec.TargetFramework.Entities
     [Serializable]
     public class MessageContainerDTO
     {
+        public bool IsCurrentUserParticipant { get; set; }
         public IEnumerable<MessageDTO> Messages { get; set; }
         public IEnumerable<ParticipantDTO> Participants { get; set; }
     }
