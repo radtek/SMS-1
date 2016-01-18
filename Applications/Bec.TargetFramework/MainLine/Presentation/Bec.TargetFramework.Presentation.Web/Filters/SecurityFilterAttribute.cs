3d40917fbe2a4f3460bf78ee4f3cde15a5814dfc
Zenon Motyka <zenonm@beconsultancy.co.uk>
2016-01-18 08:26:25 +0000

    Added the caching policy to action responses

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Filters/SecurityFilterAttribute.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Filters/SecurityFilterAttribute.cs
@@ -1,11 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 
 namespace Bec.TargetFramework.Presentation.Web.Filters
 {
-    public class SecurityFilterAttribute : ActionFilterAttribute
+    public class SecurityFilterAttribute : FilterAttribute, IResultFilter
     {
         private const string HeaderName = "Strict-Transport-Security";
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        public void OnResultExecuting(ResultExecutingContext filterContext)
         {
             var response = filterContext.HttpContext.Response;
 
@@ -16,13 +17,20 @@ namespace Bec.TargetFramework.Presentation.Web.Filters
             response.AddHeader("X-Content-Type-Options", "nosniff");
             response.AddHeader("X-Frame-Options", "SAMEORIGIN");
             response.AddHeader("X-XSS-Protection", "1; mode=block");
-            
+
             // HSTS headers should be sent via HTTPS responses only : http://tools.ietf.org/html/draft-ietf-websec-strict-transport-sec-14#section-7.2
             // They should also not be duplicated
             if (filterContext.HttpContext.Request.IsSecureConnection && filterContext.HttpContext.Response.Headers[HeaderName] == null)
                 filterContext.HttpContext.Response.AddHeader(HeaderName, "max-age=31536000; includeSubDomains; preload");
 
-            base.OnActionExecuting(filterContext);
+            // cache
+            response.Cache.SetCacheability(HttpCacheability.NoCache);  // HTTP 1.1
+            response.Cache.AppendCacheExtension("no-cache, no-store, must-revalidate"); 
+            response.AppendHeader("Pragma", "no-cache"); // HTTP 1.0.
+            response.AppendHeader("Expires", "-1"); // Proxies.
+        }
+        public void OnResultExecuted(ResultExecutedContext filterContext)
+        {
         }
     }
 }
