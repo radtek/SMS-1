1747ff27f3d061b7f75a7235aa631a05b912c540
Zenon Motyka <zenonm@beconsultancy.co.uk>
2016-01-18 13:53:12 +0000

    Add the FAQ page; Update the validation message for email not being able to register

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Controllers/HomeController.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Bec.TargetFramework.Presentation.Web.Controllers
 {
@@ -24,6 +20,11 @@ namespace Bec.TargetFramework.Presentation.Web.Controllers
         }
 
         public ActionResult BuyersAtRisk()
+        {
+            return View();
+        }
+
+        public ActionResult ConveyancingFirmFaq()
         {
             return View();
         }
