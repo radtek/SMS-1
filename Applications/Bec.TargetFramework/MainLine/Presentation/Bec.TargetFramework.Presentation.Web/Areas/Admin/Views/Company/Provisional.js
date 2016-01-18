50f071398d589bedd4e2e22400f5118f24fbd3e5
Keith Howie <keithh@beconsultancy.co.uk>
2016-01-18 13:20:31 +0000

    Company templates by state / type

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Company/Provisional.js
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Company/Provisional.js
@@ -1,4 +1,5 @@
 ﻿var uGrid, vGrid, rGrid;
+var promises;
 $(function () {
     //set up grid options for the three grids. most are passed straight on to kendo grid.
     uGrid = new gridItem(
@@ -244,114 +245,50 @@ $(function () {
     tabs.showTab($('#myTab1').data("selected"));
 
     findModalLinks();
+
+    promises = new defTmpl('Company/DetailTemplates/Provisional/',
+        ['unverified', 'verified', 'rejected', 'expired'],
+        [
+            { name: 'Conveyancer', description: 'Professional Organisation' },
+            { name: 'Broker', description: 'Mortgage Broker' },
+            { name: 'Lender', description: 'Lender' }
+        ]);
 });
 
 //data binding for the panes beneath each grid
 function unverifiedChange(dataItem) {
-    $("p#dduCompanyName").text(dataItem.Name || "");
-    $("p#dduCompanyCreatedOn").text(dateString(dataItem.CreatedOn));
-    $("p#dduCompanyCounty").text(dataItem.County || "");
-    $("p#dduCompanyPostCode").text(dataItem.PostalCode || "");
-    $("p#dduCompanyTownCity").text(dataItem.Town || "");
-    $("p#dduCompanyAddress2").text(dataItem.Line2 || "");
-    $("p#dduCompanyAddress1").text(dataItem.Line1 || "");
-    $("p#dduSystemAdminEmail").text(dataItem.OrganisationAdminEmail || "");
-    $("p#dduSystemAdminName").text((dataItem.OrganisationAdminSalutation || "") + " " + (dataItem.OrganisationAdminFirstName || "") + " " + (dataItem.OrganisationAdminLastName || ""));
-    $("p#dduReferrer").text(dataItem.Referrer || "");
-    $("p#dduSchemeID").text(dataItem.SchemeID || "");
-    $("p#ddufpm").text(dataItem.FilesPerMonth || "");
-
-    var regulatorName = dataItem.Regulator || "";
-    if (regulatorName.toLowerCase() == 'other') regulatorName = dataItem.RegulatorOther;
-    $("p#dduRegulator").text(regulatorName);
-    $("p#dduRegulatorNumber").text(dataItem.RegulatorNumber || "");
-
-    //update links
-    $("#rejectButton").data('href', $("#rejectButton").data("url") + "?orgId=" + dataItem.OrganisationID);
-    $("#verifyButton").data('href', $("#verifyButton").data("url") + "?orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
+    populateCompany(dataItem);
+    promises.unverified[dataItem.OrganisationTypeDescription].done(function (template) {
+        var html = template(dataItem);
+        console.log(dataItem);
+        $('#unverifiedPanel').html(html);
+        $("#rejectButton").data('href', $("#rejectButton").data("url") + "?orgId=" + dataItem.OrganisationID);
+        $("#verifyButton").data('href', $("#verifyButton").data("url") + "?orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
+    });
 }
 
 function verifiedChange(dataItem) {
-    $("p#ddvCompanyName").text(dataItem.Name || "");
-    $("p#ddvCompanyCreatedOn").text(dateString(dataItem.CreatedOn));
-    $("p#ddvCompanyCounty").text(dataItem.County || "");
-    $("p#ddvCompanyPostCode").text(dataItem.PostalCode || "");
-    $("p#ddvCompanyTownCity").text(dataItem.Town || "");
-    $("p#ddvCompanyAddress2").text(dataItem.Line2 || "");
-    $("p#ddvCompanyAddress1").text(dataItem.Line1 || "");
-    $("p#ddvSystemAdminEmail").text(dataItem.OrganisationAdminEmail || "");
-    $("p#ddvVerifiedPhoneNumber").text(dataItem.Notes || "");
-    $("p#ddvReferrer").text(dataItem.Referrer || "");
-    $("p#ddvSchemeID").text(dataItem.SchemeID || "");
-    $("p#ddvfpm").text(dataItem.FilesPerMonth || "");
-    
-    $("p#ddvSystemAdminName").text((dataItem.OrganisationAdminSalutation || "") + " " + (dataItem.OrganisationAdminFirstName || "") + " " + (dataItem.OrganisationAdminLastName || ""));
-
-    var regulatorName = dataItem.Regulator || "";
-    if (regulatorName.toLowerCase() == 'other') regulatorName = dataItem.RegulatorOther;
-    $("p#ddvRegulator").text(regulatorName);
-    $("p#ddvRegulatorNumber").text(dataItem.RegulatorNumber || "");
-    $("p#ddvRegisteredAsName").text(dataItem.RegisteredAsName);
-
-    $("p#ddvPINNumber").text(dataItem.PinCode);
-    $("p#ddvPINCreatedOn").text(dateString(dataItem.PinCreated));
-    
-    //update links
-    $("#pinButton").data('href', $("#pinButton").data("url") + "&orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
-
-    // toggle visibility
-    $("p#ddvPINNumber").toggle(!!dataItem.PinCode);
-    $("p#ddvPINNumber").parent().prev().toggle(!!dataItem.PinCode);
-    $("p#ddvPINCreatedOn").toggle(!!dataItem.PinCreated);
-    $("p#ddvPINCreatedOn").parent().prev().toggle(!!dataItem.PinCreated);
+    populateCompany(dataItem);
+    promises.verified[dataItem.OrganisationTypeDescription].done(function (template) {
+        var html = template(dataItem);
+        $('#verifiedPanel').html(html);
+        $("#pinButton").data('href', $("#pinButton").data("url") + "&orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
+    });
 }
 
 function rejectedChange(dataItem) {
-    $("p#ddrCompanyName").text(dataItem.Name || "");
-    $("p#ddrCompanyCounty").text(dataItem.County || "");
-    $("p#ddrCompanyPostCode").text(dataItem.PostalCode || "");
-    $("p#ddrCompanyTownCity").text(dataItem.Town || "");
-    $("p#ddrCompanyAddress2").text(dataItem.Line2 || "");
-    $("p#ddrCompanyAddress1").text(dataItem.Line1 || "");
-    $("p#ddrSystemAdminEmail").text(dataItem.OrganisationAdminEmail || "");
-    $("p#ddrSystemAdminName").text(dataItem.OrganisationAdminSalutation + " " + dataItem.OrganisationAdminFirstName + " " + dataItem.OrganisationAdminLastName);
-    $("p#ddrReferrer").text(dataItem.Referrer || "");
-    $("p#ddrSchemeID").text(dataItem.SchemeID || "");
-    $("p#ddrfpm").text(dataItem.FilesPerMonth || "");
-
-    var regulatorName = dataItem.Regulator || "";
-    if (regulatorName.toLowerCase() == 'other') regulatorName = dataItem.RegulatorOther;
-    $("p#ddrRegulator").text(regulatorName);
-    $("p#ddrRegulatorNumber").text(dataItem.RegulatorNumber || "");
-
-    $("p#ddrRejectedOn").text(dateString(dataItem.StatusChangedOn));
-    $("p#ddrRejectedBy").text(dataItem.StatusChangedBy);
-    $("p#ddrRejectedReason").text(dataItem.Reason);
-    $("p#ddrRejectedNotes").text(dataItem.Notes);
+    populateCompany(dataItem);
+    promises.rejected[dataItem.OrganisationTypeDescription].done(function (template) {
+        var html = template(dataItem);
+        $('#rejectedPanel').html(html);
+    });
 }
 
 function expiredChange(dataItem) {
-    $("p#ddeCompanyName").text(dataItem.Name || "");
-    $("p#ddeCompanyCreatedOn").text(dateString(dataItem.CreatedOn));
-    $("p#ddeCompanyCounty").text(dataItem.County || "");
-    $("p#ddeCompanyPostCode").text(dataItem.PostalCode || "");
-    $("p#ddeCompanyTownCity").text(dataItem.Town || "");
-    $("p#ddeCompanyAddress2").text(dataItem.Line2 || "");
-    $("p#ddeCompanyAddress1").text(dataItem.Line1 || "");
-    $("p#ddeSystemAdminEmail").text(dataItem.OrganisationAdminEmail || "");
-    $("p#ddeVerifiedPhoneNumber").text(dataItem.Notes || "");
-    $("p#ddeReferrer").text(dataItem.Referrer || "");
-    $("p#ddeSchemeID").text(dataItem.SchemeID || "");
-    $("p#ddefpm").text(dataItem.FilesPerMonth || "");
-
-    $("p#ddeSystemAdminName").text((dataItem.OrganisationAdminSalutation || "") + " " + (dataItem.OrganisationAdminFirstName || "") + " " + (dataItem.OrganisationAdminLastName || ""));
-
-    var regulatorName = dataItem.Regulator || "";
-    if (regulatorName.toLowerCase() == 'other') regulatorName = dataItem.RegulatorOther;
-    $("p#ddeRegulator").text(regulatorName);
-    $("p#ddeRegulatorNumber").text(dataItem.RegulatorNumber || "");
-    $("p#ddeRegisteredAsName").text(dataItem.RegisteredAsName);
-
-    //update links
-    $("#expiredPinButton").data('href', $("#expiredPinButton").data("url") + "&orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
+    populateCompany(dataItem);
+    promises.expired[dataItem.OrganisationTypeDescription].done(function (template) {
+        var html = template(dataItem);
+        $('#expiredPanel').html(html);
+        $("#expiredPinButton").data('href', $("#expiredPinButton").data("url") + "&orgId=" + dataItem.OrganisationID + "&uaoId=" + dataItem.UserAccountOrganisationID);
+    });   
 }
