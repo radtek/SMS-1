74e9a8d9f45cca4d604b93fb404eef44734a759c
Keith Howie <keithh@beconsultancy.co.uk>
2016-01-28 12:57:05 +0000

    Validation - require notes field

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Shared/_AddNotes.js
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Shared/_AddNotes.js
@@ -1,5 +1,27 @@
 ﻿ // submit from when Save button clicked
 $("#submitAddNotes").click(function () {
-    $("#submitAddNotes").prop('disabled', true)
     $("#addNotes-form").submit();
 });
+
+// Validation
+$("#addNotes-form").validate({
+    ignore: '.skip',
+    // Rules for form validation
+    rules: {
+        notes: {
+            required: true
+        }
+    },
+
+    // Do not change code below
+    errorPlacement: function (error, element) {
+        error.insertAfter(element.parent());
+    },
+
+    submitHandler: validateSubmit
+});
+
+function validateSubmit(form) {
+    $("#submitAddNotes").prop('disabled', true);
+    form.submit();
+}
