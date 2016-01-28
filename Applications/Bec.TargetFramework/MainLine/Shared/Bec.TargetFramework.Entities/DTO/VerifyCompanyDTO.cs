4feb95462f648bc8503a8cfb8f6e9c80d6f1189c
Zenon Motyka <zenonm@beconsultancy.co.uk>
2016-01-28 12:35:16 +0000

    Change verify company modal window - add more fields as editable

---
+++ Applications/Bec.TargetFramework/MainLine/Shared/Bec.TargetFramework.Entities/DTO/VerifyCompanyDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Bec.TargetFramework.Entities
+{
+    [DataContract]
+    public class VerifyCompanyDTO
+    {
+        [DataMember]
+        public Guid OrganisationID { get; set; }
+        [DataMember]
+        public Guid SroUaoID { get; set; }
+        [DataMember]
+        public string PhoneNumber { get; set; }
+        [DataMember]
+        public string OrganisationName { get; set; }
+        [DataMember]
+        public int? FilesPerMonth { get; set; }
+        [DataMember]
+        public string RegulatorNumber { get; set; }
+        [DataMember]
+        public string SroSalutation { get; set; }
+        [DataMember]
+        public string SroFirstName { get; set; }
+        [DataMember]
+        public string SroLastName { get; set; }
+        [DataMember]
+        public string SroEmail { get; set; }
+    }
+}
