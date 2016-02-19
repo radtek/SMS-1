760c8408812fd8010313ce2f1f19a1442e6ef7d9
Ha Nguyen Ngan <han@beconsultancy.co.uk>
2016-02-19 10:40:52 +0700

    Update classes based on changes in US1114, 1100

---
+++ Applications/Bec.TargetFramework/MainLine/Business/Bec.TargetFramework.Business/Logic/HelpLogicController.cs
@@ -0,0 +1,15 @@
+﻿using Bec.TargetFramework.Aop.Aspects;
+using Bec.TargetFramework.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bec.TargetFramework.Business.Logic
+{
+    [Trace(TraceExceptionsOnly = true)]
+    public class HelpLogicController : LogicBase
+    {
+    }
+}
