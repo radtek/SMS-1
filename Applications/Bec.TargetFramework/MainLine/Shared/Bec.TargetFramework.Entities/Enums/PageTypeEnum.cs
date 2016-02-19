df482c868ac20c31b719eacf95a2914a65113b5f
Ton Chu <tonc@beconsultancy.co.uk>
2016-02-19 04:39:02 +0000

    Add PageType Enum

---
+++ Applications/Bec.TargetFramework/MainLine/Shared/Bec.TargetFramework.Entities/Enums/PageTypeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bec.TargetFramework.Infrastructure.Extensions;
+
+namespace Bec.TargetFramework.Entities.Enums
+{
+    public enum PageType : int
+    {
+        [StringValue("Tour")]
+        Tour = 1,
+        [StringValue("ShowMeHow")]
+        ShowMeHow = 2,
+        [StringValue("Callout")]
+        Callout = 3
+    }
+}
