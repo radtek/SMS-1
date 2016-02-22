47bcbf51e584f1bdb238833027f8ff6b5e76a063
Ha Nguyen Ngan <han@beconsultancy.co.uk>
2016-02-22 09:50:31 +0700

    Implement Help Management page

--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/HelpController.cs
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Controllers/HelpController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Bec.TargetFramework.Entities;
+using Bec.TargetFramework.Entities.Enums;
+using Bec.TargetFramework.Infrastructure.Extensions;
+using Bec.TargetFramework.Presentation.Web.Areas.Admin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,7 +15,45 @@ namespace Bec.TargetFramework.Presentation.Web.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Types = new List<SelectListItem> {
+                new SelectListItem { Value = PageType.Tour.ToString(), Text=PageType.Tour.GetStringValue() },
+                new SelectListItem { Value = PageType.ShowMeHow.ToString(), Text=PageType.ShowMeHow.GetStringValue() },
+                new SelectListItem { Value = PageType.Callout.ToString(), Text=PageType.Callout.GetStringValue() }
+            };
+            var pageId = Guid.NewGuid();
+            TempData["HelpPageId"] = pageId.ToString();
+            TempData.Keep("HelpPageId");
             return View();
+        }
+
+        public ActionResult ViewAddHelp()
+        {
+            return PartialView("_AddHelp");
+        }
+
+        public async Task<ActionResult> GetHelps(PageType? type)
+        {
+            TempData["CurrentType"] = type;
+            var pageId = Guid.Parse(TempData["HelpPageId"].ToString());
+            TempData.Keep("HelpPageId");
+            var list = new List<HelpPageDTO> { 
+                new HelpPageDTO { HelpPageID = pageId, CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, PageName = "Test", PageUrl = "/Admin/Test", PageType = PageType.Tour.GetIntValue() } ,
+                new HelpPageDTO { HelpPageID = pageId, CreatedOn = DateTime.Now, ModifiedOn = DateTime.Now, PageName = "Test 1", PageUrl = "/Admin/Test1", PageType = PageType.Tour.GetIntValue() } 
+            };
+            var jsonData = new { Count = list.Count, Items = list };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> GetHelpItems(Guid? pageId)
+        {
+            pageId = Guid.Parse(TempData["HelpPageId"].ToString());
+            var list = new List<HelpItemDTO>
+            {
+                new HelpItemDTO { HelpItemID = Guid.NewGuid(), HelpPageID = pageId.Value, Title = "Test 1" },
+                new HelpItemDTO { HelpItemID = Guid.NewGuid(), HelpPageID = pageId.Value, Title = "Test 1" }
+            };
+            var jsonData = new { IsEmpty = list.Count == 0, Items = list, IsSortable = false };
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Help/Index.js
+++ Applications/Bec.TargetFramework/MainLine/Presentation/Bec.TargetFramework.Presentation.Web/Areas/Admin/Views/Help/Index.js
@@ -1,1 +1,115 @@
-﻿
+﻿$(function () {
+    var btnDelete = $("#btnDeleteHelp");
+    var btnEdit = $("#btnEditHelp");
+    var helpItemList = $("#helpItemList");
+    var typeDropdown = $("#typeList");
+
+    var urls = {
+        deleteHelpUrl: btnDelete.data("url"),
+        editHelpUrl: btnEdit.data("url"),
+        getHelpsUrl: helpItemList.data("helps-url"),
+        templateUrl: helpItemList.data("template-url"),
+        getHelpItemsUrl: helpItemList.data("items-url")
+    }
+
+    var currentHelp = {
+        id: null,
+        type: null
+    }
+    var helpItemsTemplatePromise = getTemplatePromise('_ItemList');
+
+    function getTemplatePromise(viewName) {
+        var def = $.Deferred();
+        ajaxWrapper({
+            url: urls.templateUrl,
+            data: {
+                view: getRazorViewPath(viewName, 'Help', 'Admin')
+            }
+        }).then(function (res) {
+            def.resolve(Handlebars.compile(res));
+        });
+        return def;
+    }
+
+    var helpGrid = new gridItem(
+        {
+            gridElementId: 'helpGrid',
+            url: $('#helpGrid').data("url"),
+            schema: { data: "Items", total: "Count", model: { id: "HelpPageID" } },
+            defaultSort: { field: "PageName", dir: "asc" },
+            panels: ['helpDetailPanel'],
+            change: onPageChange,
+            jumpToId: $('#helpGrid').data("jumpto"),
+            extraParameters: function () {
+                return "&type=" + typeDropdown.val()
+            },
+            columns: [
+                    {
+                        field: "HelpPageID",
+                        hidden: true,
+                    },
+                    {
+                        field: "PageName",
+                        title: "Page Name",
+                    },
+                    {
+                        field: "PageUrl",
+                        title: "Page URL"
+                    },
+                    {
+                        field: "PageType",
+                        title: "Page Type",
+                    },
+                    {
+                        field: "CreatedOn",
+                        title: "Created On",
+                        template: function (dataItem) { return dateString(dataItem.CreatedOn); }
+                    },
+                    {
+                        field: "ModifiedOn",
+                        title: "Modified On",
+                        template: function (dataItem) { return dateString(dataItem.ModifiedOn); }
+                    }
+            ]
+        });
+
+    var tabs = new tabItem("helpTab",
+    {
+        helpGridWrapper: {
+            grids: [helpGrid]
+        }
+    });
+    tabs.makeTab();
+    tabs.showTab($('#helpTab').data("selected"));
+    findModalLinks();
+
+    function loadItemForPage(pageId, url) {
+        var ajaxOptions = {
+            url: url,
+            data: { pageId: pageId },
+            cache: false
+        };
+        ajaxWrapper(ajaxOptions)
+            .then(function (result) {
+                var templateData = {
+                    data: result
+                };
+                helpItemsTemplatePromise.done(function (template) {
+                    var html = template(templateData);
+                    helpItemList.html(html);
+                });
+            });
+    }
+
+    function onPageChange(dataItem) {
+        $("p#ddnName").text(dataItem.PageName);
+        $("p#ddnUrl").text(dataItem.PageUrl || "");
+        $("p#ddnType").text((dataItem.PageType == 1 ? "Tour" : (dataItem.PageType == 2 ? "Show Me How" : "Callout")) || "");
+        $("p#ddnCreatedOn").text(dateString(dataItem.CreatedOn) || "");
+        $("p#ddnModifiedOn").text(dateString(dataItem.ModifiedOn) || "");
+        btnEdit.data('href', urls.editHelpUrl + "?pageId=" + dataItem.HelpPageID);
+        btnDelete.data('href', urls.deleteHelpUrl + "?pageId=" + dataItem.HelpPageID);
+        loadItemForPage(dataItem.PageId, urls.getHelpItemsUrl);
+    }
+});
+
